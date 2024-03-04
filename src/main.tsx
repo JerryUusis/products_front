ebadca98ff646b5ec7d0be589679842e882a947c
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-04 18:24:41 +0200

    Add cssbaseline

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { ThemeProvider } from '@emotion/react'
-import { createTheme } from '@mui/material'
+import { createTheme, CssBaseline } from '@mui/material'
 import { amber } from '@mui/material/colors';
 
 const theme = createTheme({
@@ -28,6 +28,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <CssBaseline />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
