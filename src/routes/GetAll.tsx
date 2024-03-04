54c6ab9acd6775b05abf3f7b5b4cf8ede43bf70f
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-04 16:54:15 +0200

    Remove unused code

--- src/routes/GetAll.tsx
+++ src/routes/GetAll.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { getAll, getKeys } from "../services/productsService";
 import { ProductType } from "../types/product";
-import { Link } from "react-router-dom";
 import Loading from "../components/Loading";
 import HomeButton from "../components/HomeButton";
 
