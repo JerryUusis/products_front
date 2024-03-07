4150cb495b6a38c64e99829106be425812f0f872
JerryUusis <jerry.uusitalo@gmail.com>
2024-03-07 16:48:43 +0200

    Refactor code

--- src/components/Validation.tsx
+++ src/components/Validation.tsx
@@ -1,42 +1,23 @@
 import { Alert, Fade } from "@mui/material";
 
 interface validationProps {
-  success: boolean;
-  successMessage: string;
-  failure: boolean;
-  failureMessage: string;
+  visible: boolean;
+  message: string;
+  alertType: "error" | "success";
 }
 
-const Validation = ({
-  success,
-  successMessage,
-  failure,
-  failureMessage,
-}: validationProps) => {
+const Validation = ({ alertType, message, visible }: validationProps) => {
   return (
     <>
-      {success ? (
-        <Fade in={success} timeout={250}>
-          <Alert
-            severity="success"
-            variant="filled"
-            sx={{ position: "absolute", zIndex: 10, width:"10rem"}}
-          >
-            {successMessage}
-          </Alert>
-        </Fade>
-      ) : null}
-      {failure ? (
-        <Fade in={failure} timeout={250}>
-          <Alert
-            severity="error"
-            variant="filled"
-            sx={{ position: "absolute", zIndex: 10, width:"10rem"}}
-          >
-            {failureMessage}
-          </Alert>
-        </Fade>
-      ) : null}
+      <Fade in={visible} timeout={250}>
+        <Alert
+          severity={alertType}
+          variant="filled"
+          sx={{ position: "absolute", zIndex: 10, width: "10rem" }}
+        >
+          {message}
+        </Alert>
+      </Fade>
     </>
   );
 };
